62d94b69da2a58fe331fce51492b7b1b929f4d0b
Kyle Meserve <KyleMeserve@Kyles-MacBook-Air.local>
2018-11-22 13:09:24 -0500

    added events to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,7 @@ CREATE TABLE users (id SERIAL, username VARCHAR(20), password VARCHAR(100), admi
 INSERT INTO users (username, password, admin) VALUES ('KyleRM', 'test', true);
 INSERT INTO users (username, password, admin) VALUES ('test', 'test', false);
 SELECT * FROM users;
+
+CREATE TABLE events (id SERIAL, location VARCHAR(50), venue VARCHAR(50), price INT, venue_contact VARCHAR(20), description VARCHAR(255));
+INSERT INTO events (location, venue, price, venue_contact, description) VALUES ('Test Location', 'Test Venue', 15, 'Test venue contact', 'A test description of the event, which really does not have to mean anything but I want it to be long.');
+SELECT * FROM events;
