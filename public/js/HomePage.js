89b63ab71440c0958a29a0964811a3f4ab319981
Kyle Meserve <KyleMeserve@Kyles-MacBook-Air.local>
2018-11-25 11:51:07 -0500

    messed with css on footer, does not overlap anything now

--- public/js/HomePage.js
+++ public/js/HomePage.js
@@ -1,20 +1,30 @@
 class HomePage extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            displayHomePage: true
+        }
+    }
     render(){
         return(
             <div>
                 <Header></Header>
-                <main>
-                    <div className="homepage-background">
-                        <div>
-                            <h3>News:</h3>
-                            <p>News would go here.</p>
+                {this.state.displayHomePage
+                    ?
+                    <main>
+                        <div className="homepage-background">
+                            <div>
+                                <h3>News:</h3>
+                                <p>News would go here.</p>
+                            </div>
+                            <div className="fb-page" data-href="https://www.facebook.com/AmarilloSkyBand/" data-tabs="timeline" data-height="500" data-small-header="false" data-adapt-container-width="false" data-hide-cover="false" data-show-facepile="false"><blockquote cite="https://www.facebook.com/AmarilloSkyBand/" className="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/AmarilloSkyBand/">Amarillo Sky</a></blockquote></div>
                         </div>
-                        <div className="fb-page" data-href="https://www.facebook.com/AmarilloSkyBand/" data-tabs="timeline" data-height="500" data-small-header="false" data-adapt-container-width="false" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/AmarilloSkyBand/" className="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/AmarilloSkyBand/">Amarillo Sky</a></blockquote></div>
-                    </div>
-                </main>
+                    </main>
+                    :
+                    ''
+                }
                 <Footer></Footer>
             </div>
-
         )
     }
 }
