2e74a4eab33b7d499119d94f19317689cf89a795
Kyle Meserve <KyleMeserve@Kyles-MacBook-Air.local>
2018-12-15 17:30:26 -0500

    Made edit for Jerry

--- public/js/InputList.js
+++ public/js/InputList.js
@@ -10,7 +10,7 @@ class InputList extends React.Component {
                 <p><span class="bold">Bassist, Backup Vocals (Rob Harrier)</span> - Stage Right</p>
                 <p><span class="bold">Guitarist (Adam Sarna)</span> - Stage Left</p>
                 <h3 class="title">Equipment Requirements:</h3>
-                <p><span class="bold">Drummer:</span> 5-piece kit - 2 up, 1 down with a 22" kick. Rolls personal monitor system with in-ears Drummer will be bringing his own mic and stand  *Backup vocalist throughout show and lead vocalist on one song</p>
+                <p><span class="bold">Drummer:</span> Pearl Custom Session, Ludwig and Yamaha 5 pc. drum kits (depending on venue), Rolls personal in-ear monitor system, Alesis drum sampler, and an EV microphone and stand. He sings lead and backup harmonies throughout the show.</p>
                 <p><span class="bold">Lead Vocalist:</span> Will need a mic and stand. Does not play an instrument. Uses in-ears.</p>
                 <p><span class="bold">Bassist:</span> Ampeg 1000 watt head (direct out) with Ampeg 4x10 and 1x15 cabinets. Active 5-string bass. Will need floor monitor (1/2 guitar through monitors and 3/4 vocals through monitors) Will need mic and stand for backup vocals</p>
                 <p><span class="bold">Guitarist:</span> 212 Blackstar amp. Direct box for acoustic guitar run through PA. Uses drop tune pedal and pedal board that came with amplifier.  Has a guitar monitor but will need a floor monitor as well. Does not need a mic.</p>
