478b091ba42d4ae583fd7f81e65cb2cdfcb9816d
KRMeserve <krmeserve@gmail.com>
2019-02-15 11:41:44 -0500

    changed form to be easier to read

--- public/js/EventsForm.js
+++ public/js/EventsForm.js
@@ -37,13 +37,13 @@ class EventsForm extends React.Component {
             <div class="article">
                 <h1 class="title">Enter Event Information:</h1>
                 <form onSubmit={this.handleSubmit}>
-                    <label>Event Day:</label><br/>
+                    <label>Event Date (ex: July 1, 2019):</label><br/>
                     <input onChange={this.handleChange} id="event_day" type="text" value={this.state.event_day}></input><br/>
-                    <label>Location:</label><br/>
+                    <label>Venue Name (ex: Czars - Don't use apostrophes):</label><br/>
                     <input onChange={this.handleChange} id="location" type="text" value={this.state.location}></input><br/>
-                    <label>Venue:</label><br/>
+                    <label>City and State (ex: Middlebury, IN):</label><br/>
                     <input onChange={this.handleChange} id="venue" type="text" value={this.state.venue}></input><br/>
-                    <label>Duration:</label><br/>
+                    <label>Hours: (ex: 8:00pm-12:00am)</label><br/>
                     <input onChange={this.handleChange} id="duration" type="text" value={this.state.duration}></input><br/>
                     <input type="submit" value="Submit"></input>
                 </form>
